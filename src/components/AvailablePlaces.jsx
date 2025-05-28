ae302455de3457ff96f306c955674a988427739a
Sagar Sengupta <sagarsengupta2009@gmail.com>
2025-05-28 21:54:23 +0530

    first commit

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
+    };
+
+    fetchPlaces();
+  }, []);
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching places data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
